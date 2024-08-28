1dd8750cbdfd4346e812218b3a86a5cc4fa95285
avrilyql <avrilyql2027@student.cis.edu.hk>
2024-08-28 10:01:40 +0800

    Finish Quiz 1

--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,22 @@ import java.util.Scanner;
 *****************************************************************************************************
 * By writing your name in the space provided below, you are honouring the CIS academic honesty pledge 
 * “I have neither given nor received unauthorized aid on this piece of work.”
-* NAME: 
+* NAME: Avril
 *****************************************************************************************************
 */
 
 public class Main {
 
     public static void main(String[] args) {
-
-
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your age: ");
+        int age = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Enter your first name: ");
+        String firstName = scanner.nextLine();
+        System.out.println("Enter your favourite food: ");
+        String favouriteFood = scanner.nextLine();
+        System.out.println("First name: " + firstName + "\nAge: " + age + "\nFavourite Food: " + favouriteFood);
 
     }
 
